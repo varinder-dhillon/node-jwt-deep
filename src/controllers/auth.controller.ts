dd6feaab31892610dbd1619588c1a0653e9a0d24
Varinder Singh <varinderdhilloninfo@gmail.com>
2025-11-28 23:11:38 +0530

    Feat: Completed prisma integration and register API

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,22 @@
-import { catchAsync } from "../utils/catchAsync"
+import * as bcrypt from "bcryptjs";
+import { catchAsync } from "../utils/catchAsync";
+import { prisma } from "../config/database";
 
 
 export const register = catchAsync(async (req, res, next) => {
-    res.json({ message: "User registered" });
+    const { name, email, password } = req.body;
+
+    // Hash Password 
+    const hashedPass:string = await bcrypt.hash(password, 15)
+
+    // Create user in db 
+    const user = await prisma.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPass
+        }
+    })
+
+    res.json({ message: "User registered", data: {user}});
 })
