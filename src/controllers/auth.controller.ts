f3d70e8a748b2ae3112a7918fcb2b106fca2ae67
Varinder Singh <varinderdhilloninfo@gmail.com>
2025-11-28 23:23:08 +0530

    Feat: Added the login API

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -20,3 +20,23 @@ export const register = catchAsync(async (req, res, next) => {
 
     res.json({ message: "User registered", data: {user}});
 })
+
+export const login = catchAsync(async (req, res, next) => {
+    const {email, password} = req.body;
+
+    // Check user 
+    const user = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    })
+
+    if(!user) res.status(404).json({message: "User not found!"})
+
+    // Check user pass 
+    const checkPass = await bcrypt.compare(password, user?.password || "");
+
+    if (!checkPass) res.status(404).json({message: "Email/Password is wrong!"});
+
+    res.status(200).json({message: "Logined successfully!", data: user})
+})
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { register } from "../controllers/auth.controller";
+import { register, login } from "../controllers/auth.controller";
 
 const authRouter = express.Router();
 
-authRouter.post("/register", register)
+authRouter.post("/register", register);
+authRouter.post("/login", login)
 
 
 export default authRouter;
